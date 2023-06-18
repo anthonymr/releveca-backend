caab5c3fee142bf5100ddb206e93db716b5d4ef9
Anthony Martin <anthonymartin20@gmail.com>
2023-06-18 09:55:00 -0400

    Fix Rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, length: { minimum: 8, maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 8, maximum: 50 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6, maximum: 20 }
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :last_name, presence: true, length: { minimum: 3, maximum: 50 }
