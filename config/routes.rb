91b794100afb5b36b8a9b7c25fff1954db2a027d
Anthony Martin <anthonymartin20@gmail.com>
2023-06-19 15:01:36 -0400

    Add item routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,19 @@ Rails.application.routes.draw do
       get 'users/corporations', to: 'users#corporations', as: 'user_corporations'
       post 'users/corporations', to: 'users#add_corporation', as: 'user_add_corporation'
       put 'users', to: 'users#update', as: 'current_user_update'
-      patch 'users/:id', to: 'users#change_status', as: 'user_change_status'
+      patch 'users/:id/status', to: 'users#change_status', as: 'user_change_status'
       resources :users, except: %i[new edit update destroy]
 
       get 'corporations/current', to: 'corporations#current', as: 'current_corporation'
       post 'corporations/current', to: 'corporations#select', as: 'select_corporation'
+      get 'corporations/items', to: 'corporations#items', as: 'corporation_items'
       put 'corporations', to: 'corporations#update', as: 'update_corporation'
-      patch 'corporations/:id', to: 'corporations#change_status', as: 'corporation_change_status'
+      patch 'corporations/:id/status', to: 'corporations#change_status', as: 'corporation_change_status'
       resources :corporations, except: %i[new destroy update edit]
+
+      patch 'items/:id/status', to: 'items#change_status', as: 'item_change_status' # implementar
+      patch 'items/:id/stock', to: 'items#change_stock', as: 'item_change_stock' # implementar
+      resources :items, except: %i[new edit destroy] # implementar
 
       post 'auth', to: 'authentication#create'
       get 'auth', to: 'authentication#show'
