82c7883be5e0b9d107cf733a53c55d1e9f5222fc
Anthony Martin <anthonymartin20@gmail.com>
2023-06-19 15:19:58 -0400

    Add index, show and create to Items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,28 @@
 class ItemsController < ApplicationController
+  def index
+    return forbidden('First select a corporation') unless Setting.corporation
+
+    ok(Setting.corporation.items, 'Items retrieved successfully')
+  end
+
+  def show
+    return forbidden('First select a corporation') unless Setting.corporation
+
+    ok(Setting.corporation.items.find(params[:id]), 'Item retrieved successfully')
+  rescue ActiveRecord::RecordNotFound
+    not_found('Item not found')
+  end
+
+  def create
+    return forbidden('First select a corporation') unless Setting.corporation
+
+    item = Setting.corporation.items.new(item_params)
+    item.save ? ok(item, 'Item created successfully') : bad_request(item.errors)
+  end
+
+  private
+
+  def item_params
+    params.permit(:code, :name, :model, :stock, :unit, :price, :index)
+  end
 end
