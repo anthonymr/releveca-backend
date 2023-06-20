7e3ce1260ac51182e9dc70003fd58b04f8b214b2
Anthony Martin <anthonymartin20@gmail.com>
2023-06-20 11:07:04 -0400

    Create client model

---
+++ app/models/client.rb
@@ -0,0 +1,20 @@
+class Client < ApplicationRecord
+  belongs_to :user
+  belongs_to :corporation
+
+  validates :code, presence: true, length: { maximum: 50 }, uniqueness: { scope: :corporation_id }
+  validates :type, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :phone, presence: true, length: { maximum: 50 }
+  validates :status, presence: true, length: { maximum: 50 }, inclusion: { in: %w[enabled disabled] }
+  validates :notes, length: { maximum: 500 }
+  validates :address, presence: true, length: { maximum: 500 }
+  validates :rif, presence: true, length: { maximum: 15 }, uniqueness: { scope: :corporation_id },
+                  format: { with: /\A[VEJPG]{1}[0-9]{5,9}\z/ }
+  validates :taxpayer, inclusion: { in: [true, false] }
+  validates :nit, length: { maximum: 15 }
+  validates :email, length: { maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :index, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :country_id, presence: true
+  validates :user_id, presence: true
+end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,4 @@
 class Country < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: /\A[A-Z\s]+\z/, message: "only allows uppercase and spaces" }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 },
+                   format: { with: /\A[A-Z\s]+\z/, message: 'only allows uppercase and spaces' }
 end
