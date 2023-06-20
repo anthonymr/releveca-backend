df6b96460c3aa69897f19a318295ac354452c67f
Anthony Martin <anthonymartin20@gmail.com>
2023-06-20 07:28:10 -0400

    Create countries schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_184621) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_112441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_184621) do
     t.bigint "user_id"
     t.index ["corporation_id"], name: "index_corporations_users_on_corporation_id"
     t.index ["user_id"], name: "index_corporations_users_on_user_id"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_countries_on_name", unique: true
   end
 
   create_table "items", force: :cascade do |t|
