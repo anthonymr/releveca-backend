16223b2bfa33005ab5f1afa6596ba279b435396c
Anthony Martin <anthonymartin20@gmail.com>
2023-07-21 09:29:33 -0400

    Rename paginated to pagination

--- app/models/concerns/paginable.rb
+++ app/models/concerns/paginable.rb
@@ -7,15 +7,15 @@ module Paginable
     next_page = sliced.size < real_page.next.next ? real_page.next.next : real_page.next
     previous_page = real_page.zero? ? 0 : real_page
 
-    paginated = {
+    pagination = {
       pages: sliced.size,
       current_page: page.to_i,
       next_page: next_page.to_s,
       previous_page: previous_page.to_s
     }
 
-    return { paginated:, payload: sliced.last } if real_page.next > sliced.size
+    return { pagination:, payload: sliced.last } if real_page.next > sliced.size
 
-    { paginated:, payload: items.each_slice(count).to_a[page.to_i - 1] }
+    { pagination:, payload: items.each_slice(count).to_a[page.to_i - 1] }
   end
 end
