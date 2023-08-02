0e81f4ce23004153196cbc3886b586309ec3566a
Anthony Martin <anthonymartin20@gmail.com>
2023-08-02 08:50:37 -0400

    Move api_helper to the support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'api_helper'
+require 'support/api_helper'
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
